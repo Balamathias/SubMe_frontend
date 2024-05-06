b425158cc628fcae7e1fa7f71b85b53732a79f61
Balamathias <balamathias05@gmail.com>
2024-05-06 15:07:47 +0100

    implement bug fixes on login

--- src/components/dashboard/TopNavbar.tsx
+++ src/components/dashboard/TopNavbar.tsx
@@ -1,26 +1,29 @@
-import Image from 'next/image'
-import React from 'react'
-import Avatar from '../Avatar'
-import { getUser } from '@/lib/supabase/accounts'
-import Link from 'next/link'
+'use client'
 
-const TopNavbar = async() => {
-    const { data: accountData } = await getUser()
+import React from 'react'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import { ArrowRightCircle } from 'lucide-react'
+import Image from 'next/image'
+
+const TopNavbar = () => {
   return (
-    <div className='md:hidden flex fixed top-0 z-20 h-16 justify-between bg-secondary dark:bg-slate-950 items-center w-full p-3 border-b-2 border-dotted shadom-sm'>
+    <div className='md:hidden flex fixed top-0 z-20 h-10 justify-between'>
         <Link href='/dashboard'>
             <Image
             src='/vitals/logo-192.png'
             alt='Logo'
-            width={50}
-            height={50}
+            width={40}
+            height={40}
             quality={100}
-            className="h-10 w-10 ml-4 object-contain"
+            className="h-8 w-8 ml-4 object-contain"
             />
         </Link>
 
         <nav className="flex flex-col items-center justify-center">
-            <Avatar name={accountData?.username || ''} />
+            <Button size={'icon'} variant={'ghost'}>
+                <ArrowRightCircle size={12} className='text-rose-800' />
+            </Button>
         </nav>
     </div>
   )
