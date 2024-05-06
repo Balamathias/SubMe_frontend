545733af300507986ea76b40ba039f0b5f307b10
Balamathias <balamathias05@gmail.com>
2024-05-06 15:18:00 +0100

    implement bug fixes on login

--- src/components/dashboard/MobileBottombar.tsx
+++ src/components/dashboard/MobileBottombar.tsx
@@ -6,7 +6,7 @@ import MobileLinkItem from './MobileLinkItem'
 
 const MobileBottombar = () => {
   return (
-    <div className="md:hidden fixed bottom-0 w-full flex flex-1 gap-2 z-20 bg-secondary dark:bg-slate-950 border-t-2 border-dashed h-16">
+    <div className="md:hidden fixed bottom-0 w-full flex flex-1 gap-2 z-20 bg-background shadow-md drop-shadow-md dark:bg-slate-950 dark:border-t-2 border-dashed h-16">
         <nav className="flex justify-between gap-1.5 w-full items-center p-3">
             {
                 links.map((link, index) => (
