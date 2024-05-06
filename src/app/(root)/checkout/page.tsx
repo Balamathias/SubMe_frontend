398e5db9afa3cffdeb7cca23d39837ad145295f8
Balamathias <balamathias05@gmail.com>
2024-05-06 06:31:09 +0100

    Fix bugs for prod

--- src/app/(root)/checkout/page.tsx
+++ src/app/(root)/checkout/page.tsx
@@ -4,7 +4,7 @@ import PaymentForm from "@/components/remita/PaymentForm";
 export default function CheckoutPage () {
     return (
         <WidthWrapper className="min-h-screen items-center justify-center">
-            <PaymentForm />
+           Checkout
         </WidthWrapper>
     )
 }
