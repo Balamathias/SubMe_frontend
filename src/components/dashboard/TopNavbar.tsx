77af4800ef30c56b8dd5e083603db66d06c7ad28
Balamathias <balamathias05@gmail.com>
2024-05-06 15:12:59 +0100

    implement bug fixes on login

--- src/components/dashboard/TopNavbar.tsx
+++ src/components/dashboard/TopNavbar.tsx
@@ -8,7 +8,7 @@ import Image from 'next/image'
 
 const TopNavbar = () => {
   return (
-    <div className='md:hidden flex fixed top-0 z-20 h-10 justify-between'>
+    <div className='md:hidden flex fixed top-0 z-20 h-12 justify-between w-full p-3'>
         <Link href='/dashboard'>
             <Image
             src='/vitals/logo-192.png'
@@ -16,13 +16,13 @@ const TopNavbar = () => {
             width={40}
             height={40}
             quality={100}
-            className="h-8 w-8 ml-4 object-contain"
+            className="h-10 w-10 ml-4 object-contain"
             />
         </Link>
 
         <nav className="flex flex-col items-center justify-center">
             <Button size={'icon'} variant={'ghost'}>
-                <ArrowRightCircle size={12} className='text-rose-800' />
+                <ArrowRightCircle size={17} className='text-rose-800 font-semibold' />
             </Button>
         </nav>
     </div>
