32ee73041e8a5a78b65e45b9bbd5d3e93e35f37e
Balamathias <balamathias05@gmail.com>
2024-05-07 13:30:54 +0100

    Implement fixaation

--- src/components/DynamicSheet.tsx
+++ src/components/DynamicSheet.tsx
@@ -24,8 +24,10 @@ const DynamicSheet = ({trigger, children, className}: DynamicSheetProps) => {
         <SheetTrigger asChild>
             {trigger}
         </SheetTrigger>
-        <SheetContent className={clsx('flex flex-col gap-3 overflow-hidden border-none rounded-md drop-shadow-md shadow-md p-3 py-5', className)}>
-           {children}
+        <SheetContent className={clsx('flex flex-col gap-3 overflow-hidden border-none rounded-md drop-shadow-md shadow-md px-2 py-5 custom-scrollbar', className)}>
+           <div className="flex flex-col p-4 px-1 overflow-auto custom-scrollbar">
+            {children}
+           </div>
         </SheetContent>
     </Sheet>
   )
