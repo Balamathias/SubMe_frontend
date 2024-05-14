2c4241ce59feb66a1a29df02660342bdd3123c07
Balamathias <balamathias05@gmail.com>
2024-05-14 02:09:06 +0100

    OAuth

---
+++ src/app/auth/auth-code-error/page.tsx
@@ -0,0 +1,19 @@
+import WidthWrapper from '@/components/WidthWrapper'
+import { buttonVariants } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <WidthWrapper>
+      <div className="h-screen overflow-hidden w-full flex items-center justify-center flex-col">
+        <Image src="/glass/images/clip.png" width={150} height={150} className='object-cover' alt="Error" />
+        <p className="text-lg text-muted-foreground">Seems like the link you followed hs expired, Please try again...</p>
+        <Link className={buttonVariants()} href={'/sign-in'}>Return to Sign In</Link>
+      </div>
+    </WidthWrapper>
+  )
+}
+
+export default ErrorPage
