545e02156bd5cf32bee56b1b46e742f612bc4341
Balamathias <balamathias05@gmail.com>
2024-05-14 18:31:46 +0100

    Making many ssr components as soon as possible

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,14 +1,16 @@
+'use client'
+
 import { sidebarLinks } from '@/utils/sidebarLinks'
 import React from 'react'
 import LinkItem from './LinkItem'
 import Image from 'next/image'
 import Avatar from '../Avatar'
+import { useCurrentUser } from '@/providers/user-provider'
 import Link from 'next/link'
 import SignoutButton from '../SignoutButton'
-import { getUser } from '@/lib/supabase/accounts'
 
-const SidebarComponent = async () => {
-    const {data: user} = await getUser()
+const SidebarComponent = () => {
+    const user = useCurrentUser()
   return (
     <div className='h-screen md:flex flex-col p-2 lg:p-2.5 hidden w-[270px] lg:w-[300px] overflow-auto custom-scrollbar justify-between border-r-2 border-dashed'>
         <div className="flex flex-col space-y-12">
@@ -35,10 +37,10 @@ const SidebarComponent = async () => {
         <footer className="p-2 lg:p-2.5 flex gap-3 flex-col">
             <div className="flex flex-col gap-2">
                 <div className="flex items-center gap-1.5 max-lg:flex-col max-md:justify-center">
-                    <Avatar name={user?.first_name || ''} />
+                    <Avatar name={user?.currentAccountUser?.first_name || ''} />
                     <div className="flex flex-col gap-0.5 max-lg:items-center max-lg:justify-center">
-                        <h3 className='text-primary text-sm py-0 font-semibold truncate'>{user?.username || ''}</h3>
-                        <p className='text-muted-foreground text-xs truncate'>{user?.email}</p>
+                        <h3 className='text-primary text-sm py-0 font-semibold truncate'>{user?.currentAccountUser?.username || ''}</h3>
+                        <p className='text-muted-foreground text-xs truncate'>{user?.currentAccountUser?.email}</p>
                     </div>
                 </div>
                 <SignoutButton />
