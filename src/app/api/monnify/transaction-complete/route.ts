0ccd3f058774276178e2eba34db44139b372596c
Balamathias <balamathias05@gmail.com>
2024-06-22 00:53:24 +0100

    New commit

--- src/app/api/monnify/transaction-complete/route.ts
+++ src/app/api/monnify/transaction-complete/route.ts
@@ -9,7 +9,7 @@ export const POST = async (req: Request, res: Response) => {
         email: 'balamathias40@gmail.com',
         subject: 'Transaction completed',
         message: 'Transaction completed successfully',
-        html: '<h1>Transaction completed successfully</h1>'
+        html: `<pre>${JSON.stringify(data ?? [])}</pre>`
     })
 
     return NextResponse.json({message: 'Transaction completed'}, {status: 200, statusText: 'OK'})
